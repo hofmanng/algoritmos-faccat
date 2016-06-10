bd72b603820e3bc6465871400f11e692182fef36
hofmanng <hofmannguilherme@hotmail.com>
2016-06-10 18:43:12 -0300

    .

---
+++ prog90.py
@@ -0,0 +1,20 @@
+#
+# Escreva um algoritmo que pergunte ao usuario um 
+# numero e apos escreva na tela a soma total de 1 
+# ate o numero lido. Exemplo: 5 = 1 + 2 + 3 + 4 + 5
+# = 15.
+#
+
+# Declaracao das variaveis
+n = int(input("Insira um valor: "))
+soma = 0
+
+# Loop
+for a in range(1, n + 1):
+    soma = soma + a
+    print a,"+",
+else:
+    print
+
+# Saida de dados
+print "=",soma
