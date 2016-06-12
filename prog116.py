eb75a01a06e082a3eca849f54fd8186bf5ceb50f
hofmanng <hofmannguilherme@hotmail.com>
2016-06-12 04:33:58 -0300

    .

---
+++ prog116.py
@@ -0,0 +1,11 @@
+#
+# Faca um programa que imprima na tela apenas os numeros
+# impares entre 1 e 50.
+#
+
+# Processamento
+for n in range(1, 50 + 1):
+    # Teste condicional
+    if (n % 2) != 0:
+        # Saida de dados
+        print n
