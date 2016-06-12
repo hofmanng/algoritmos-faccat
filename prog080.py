0311cb4b1946d7bdd19141a9d0f7b64d8a1a9172
hofmanng <hofmannguilherme@hotmail.com>
2016-06-12 18:13:06 -0300

    .

---
+++ prog080.py
@@ -0,0 +1,9 @@
+#
+# Escreva um algoritmo para imprimir os numeros de 1 (inclusive) a 10 (inclusive)
+# em ordem crescente.
+#
+
+# Processamento
+for n in range(1, 10 + 1):
+    # Saida de dados
+    print n
---
+++ prog081.py
@@ -0,0 +1,9 @@
+#
+# Escreva um algoritmo para imprimir os 10 primeiros numeros inteiros
+# maiores que 100.
+#
+
+# Processamento
+for n in range(100, 111):
+    # Saida de dados
+    print n
